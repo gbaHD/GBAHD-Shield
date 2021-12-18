b1db1d8ba44e63cfeef22ce67a5419abe410bcb5
BBsan2k <bbsan@gmx.de>
2021-12-18 13:25:12 +0100

    Fix build by using bluepad template as component foundation

--- esp32/get_components.sh
+++ esp32/get_components.sh
@@ -1,11 +1,16 @@
 #!/bin/zsh
 
-BLUEPAD32=$(pwd)/external/bluepad32
+BLUEPAD32=$(pwd)/external/bluepad32_template
 
 # Install Bluepad32 to components
-[ ! -d components/bluepad32 ] && cp -r ${BLUEPAD32}/src/components/bluepad32 ./components
-[ ! -d components/bluepad32_arduino ] && cp -r ${BLUEPAD32}/src/components/bluepad32_arduino ./components
+#[ ! -d components/bluepad32 ] && cp -r ${BLUEPAD32}/src/components/bluepad32 ./components
+#[ ! -d components/bluepad32_arduino ] && cp -r ${BLUEPAD32}/src/components/bluepad32_arduino ./components
 
 # Setup BTStack
-IDF_PATH=$(pwd)
-python3 ${BLUEPAD32}/external/btstack/port/esp32/integrate_btstack.py
+#IDF_PATH=$(pwd)
+#python3 ${BLUEPAD32}/external/btstack/port/esp32/integrate_btstack.py
+
+#cp external/BTStack_CMakeLists.txt components/btstack/CMakeLists.txt
+#cp external/BluePad_CMP_CMakeLists.txt components/Bluepad32/CMakeLists.txt
+
+cp -r ${BLUEPAD32}/components/bluepad32* ${BLUEPAD32}/components/btstack components/
