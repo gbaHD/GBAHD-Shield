b6cb8c84eda46fc78756ad1678842de4926188a9
BBsan2k <bbsan@gmx.de>
2021-12-22 14:25:05 +0100

    Add first crude version to update firmware

--- esp32/main/mega_handler.cpp
+++ esp32/main/mega_handler.cpp
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Bluepad32.h>
+#include <SPIFFS.h>
 
 #include "mega_handler.h"
 
@@ -18,6 +19,13 @@
 #define GBA_OUT_R      (0x080)
 #define GBA_OUT_START  (0x100)
 #define GBA_OUT_SELECT (0x200)
+
+#define MEGA_BL_ADDRESS             (0x29)
+#define MEGA_VERSION_HASH_OFFSET    (0x100) // <-- This needs to be updated once we know the location
+#define MEGA_PAGE_SIZE              (0x80)  // <-- Page Size of ATMega328P
+
+#define SDA_PIN         (32)
+#define SCL_PIN         (33)
 
 GamepadPtr Mega_Handler_Class::controller;
 
@@ -37,6 +45,9 @@ void Mega_Handler_Class::onDisconnectedGamepad(GamepadPtr gp)
 void Mega_Handler_Class::update_controller()
 {
     uint16_t newOutputs = 0U;
+
+    BP32.update();
+
     if (controller && controller->isConnected()) 
     {
         MAP_BUTTON(controller->dpad(), newOutputs, DPAD_UP, GBA_OUT_UP);
@@ -44,7 +55,7 @@ void Mega_Handler_Class::update_controller()
         MAP_BUTTON(controller->dpad(), newOutputs, DPAD_LEFT, GBA_OUT_LEFT);
         MAP_BUTTON(controller->dpad(), newOutputs, DPAD_RIGHT, GBA_OUT_RIGHT);
         MAP_BUTTON(controller->buttons(), newOutputs, BUTTON_A, GBA_OUT_A);
-        MAP_BUTTON(controller->buttons(), newOutputs, BUTTON_A, GBA_OUT_B);
+        MAP_BUTTON(controller->buttons(), newOutputs, BUTTON_B, GBA_OUT_B);
         MAP_BUTTON(controller->buttons(), newOutputs, BUTTON_TRIGGER_L, GBA_OUT_L);
         MAP_BUTTON(controller->buttons(), newOutputs, BUTTON_SHOULDER_L, GBA_OUT_L);
         MAP_BUTTON(controller->buttons(), newOutputs, BUTTON_TRIGGER_R, GBA_OUT_R);
@@ -53,18 +64,117 @@ void Mega_Handler_Class::update_controller()
         MAP_BUTTON(controller->miscButtons(), newOutputs, MISC_BUTTON_BACK, GBA_OUT_SELECT);
     }
 
-    Serial.println(newOutputs);
+    Wire.beginTransmission(MEGA_BL_ADDRESS);
+    Wire.write(reinterpret_cast<uint8_t*>(&newOutputs), sizeof(uint16_t));
+    Wire.endTransmission();
+
+    if (newOutputs != 0)
+    {
+        Serial.print("Current Controls: ");
+        Serial.println(newOutputs, BIN);
+    }
+}
+
+void Mega_Handler_Class::get_update_version(String& version)
+{
+    char hash[8] = {0};
+    File atmelFile = SPIFFS.open("/atmel.hex", "r");
+
+    if (atmelFile)
+    {
+        atmelFile.seek(MEGA_VERSION_HASH_OFFSET);
+
+        atmelFile.readBytes(hash, 7);
+    }
+    version += hash;
+
+    atmelFile.close();
+}
+
+void Mega_Handler_Class::get_mega_version(String& version)
+{
+    Wire.beginTransmission(MEGA_BL_ADDRESS);
+    Wire.write(0x02);   // <-- Command
+    Wire.write(0x01);   // <-- Use FLASH
+    Wire.write(0xFF & (MEGA_VERSION_HASH_OFFSET >> 8));
+    Wire.write(0xFF & MEGA_VERSION_HASH_OFFSET);
+    Wire.endTransmission();
+    Wire.requestFrom(MEGA_BL_ADDRESS, 7); // <-- Request only 7 Byte for short commit hash
+
+    while (Wire.available()) {
+        version += static_cast<char>(Wire.read());
+    }
+
+    Wire.endTransmission();
+}
+
+void Mega_Handler_Class::stop_bootloader()
+{
+    Wire.beginTransmission(MEGA_BL_ADDRESS);
+    Wire.write(0x00);
+    Wire.endTransmission();
+}
+
+void Mega_Handler_Class::start_application()
+{
+    Wire.beginTransmission(MEGA_BL_ADDRESS);
+    Wire.write(0x01);
+    Wire.write(0x80);
+    Wire.endTransmission();
+}
+
+void Mega_Handler_Class::update_mega()
+{
+    String oldVersion = "", newVersion = "";
+    get_mega_version(oldVersion);
+    get_update_version(newVersion);
+    if (oldVersion != newVersion)
+    {
+        File atmelFile = SPIFFS.open("/atmel.hex", "r");
+        uint16_t write_address = 0;
+
+        char buffer[MEGA_PAGE_SIZE] = {0};
+        
+        while (atmelFile.readBytes(buffer, MEGA_PAGE_SIZE) > 0)
+        {
+            Wire.beginTransmission(MEGA_BL_ADDRESS);
+            Wire.write(0x02);
+            Wire.write(0x01);
+
+            Wire.write(0xFF & (write_address >> 8));
+            Wire.write(0xFF & write_address);
+
+            for (uint8_t idx = 0U; idx < MEGA_PAGE_SIZE; idx++)
+            {
+                Wire.write(buffer[idx]);
+            }
+
+            Wire.endTransmission();
+
+            write_address += MEGA_PAGE_SIZE;
+        }
+
+        atmelFile.close();
+    }
 }
 
 
 void Mega_Handler_Class::init()
 {
-  String fv = BP32.firmwareVersion();
-  Serial.print("Bluepad32 Firmware: ");
-  Serial.println(fv);
+    String fv = BP32.firmwareVersion();
+    Serial.print("Bluepad32 Firmware: ");
+    Serial.println(fv);
 
-  // Setup the Bluepad32 callbacks
-  BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
+    // Setup the Bluepad32 callbacks
+    BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
+
+    Wire.begin(SDA_PIN, SCL_PIN);
+
+    stop_bootloader();
+
+    update_mega();
+
+    start_application();
 }
 
 void Mega_Handler_Class::update()
--- esp32/main/mega_handler.h
+++ esp32/main/mega_handler.h
@@ -9,11 +9,13 @@ class Mega_Handler_Class
     public:
         void init();
         void update();
-        void get_mega_version(uint8_t& major, uint8_t& minor);
-        void get_update_version(uint8_t& major, uint8_t& minor);
+        void get_mega_version(String& version);
+        void get_update_version(String& version);
     private:
         void update_controller();
         void update_mega();
+        void stop_bootloader();
+        void start_application();
         static void onConnectedGamepad(GamepadPtr gp);
         static void onDisconnectedGamepad(GamepadPtr gp);
         static GamepadPtr controller;
