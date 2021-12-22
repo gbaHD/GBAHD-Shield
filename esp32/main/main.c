929609113744ee71c1f33a2947ec08ffb823e236
BBsan2k <bbsan@gmx.de>
2021-12-22 14:25:30 +0100

    Remove Bluepad Debug outputs

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -29,15 +29,7 @@ limitations under the License.
 int app_main(void) {
   // hci_dump_open(NULL, HCI_DUMP_STDOUT);
 
-#ifdef CONFIG_BLUEPAD32_UART_OUTPUT_DISABLE
-  // Adafruit Airlift modules have the UART RX/TX (GPIO 1 / 3) wired with the
-  // controller so they can't be used for logging. In fact they can generate
-  // noise and can break the communication with the controller. That's why it is
-  // disabled by default.
   uni_esp32_enable_uart_output(0);
-#else
-  uni_esp32_enable_uart_output(1);
-#endif
 
   // Configure BTstack for ESP32 VHCI Controller
   btstack_init();
