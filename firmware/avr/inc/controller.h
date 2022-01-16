b02f01a485fd87a247d0abb285a94d0d03cf16c9
ManCloud <ch_ruecker@t-online.de>
2022-01-16 18:20:05 +0100

    added bt_controller connected flag

--- firmware/avr/inc/controller.h
+++ firmware/avr/inc/controller.h
@@ -16,19 +16,20 @@
 /* === TYPES ================================================================ */
 
 /* === MACROS =============================================================== */
-#define CTRL_IN_A      (0x008)
-#define CTRL_IN_B      (0x800)
-#define CTRL_IN_Y      (0x400)
-#define CTRL_IN_X      (0x004)
-#define CTRL_IN_UP     (0x080)
-#define CTRL_IN_DOWN   (0x040)
-#define CTRL_IN_LEFT   (0x020)
-#define CTRL_IN_RIGHT  (0x010)
-#define CTRL_IN_L      (0x002)
-#define CTRL_IN_R      (0x001)
-#define CTRL_IN_START  (0x100)
-#define CTRL_IN_SELECT (0x200)
-#define CTRL_DATA_MASK (0xFFF)
+#define CTRL_IN_A           (0x008)
+#define CTRL_IN_B           (0x800)
+#define CTRL_IN_Y           (0x400)
+#define CTRL_IN_X           (0x004)
+#define CTRL_IN_UP          (0x080)
+#define CTRL_IN_DOWN        (0x040)
+#define CTRL_IN_LEFT        (0x020)
+#define CTRL_IN_RIGHT       (0x010)
+#define CTRL_IN_L           (0x002)
+#define CTRL_IN_R           (0x001)
+#define CTRL_IN_START       (0x100)
+#define CTRL_IN_SELECT      (0x200)
+#define CTRL_DATA_MASK      (0xFFF)
+#define CTRL_BT_CONNECTED   (0x8000)
 
 #define GBA_OUT_A      (0x010)
 #define GBA_OUT_B      (0x020)
@@ -43,7 +44,7 @@
 #define GBA_DATA_MASK  (0x3FF)
 
 #define COMBO_IGR      (CTRL_IN_R | CTRL_IN_L | CTRL_IN_START | CTRL_IN_SELECT)
-#define COMBO_AVR_RST  (CTRL_IN_R | CTRL_IN_L | CTRL_IN_START | CTRL_IN_SELECT | CTRL_IN_A | CTRL_IN_X | CTRL_IN_LEFT | CTRL_IN_RIGHT)
+#define COMBO_AVR_RST  (CTRL_IN_R | CTRL_IN_L | CTRL_IN_START | CTRL_IN_SELECT | CTRL_IN_A | CTRL_IN_Y | CTRL_IN_LEFT | CTRL_IN_RIGHT)
 #define ENABLE_OSD     (CTRL_IN_UP | CTRL_IN_R | CTRL_IN_L | CTRL_IN_SELECT)
 
 /* === GLOBALS ============================================================== */
