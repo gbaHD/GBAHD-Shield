54fdaf5a13c8b80dff8cdd578d17e1864414b11d
BBsan2k <bbsan@gmx.de>
2024-01-30 14:14:52 +0100

    Fix Shutdown Loop

--- firmware/esp32/main/arduino_main.cpp
+++ firmware/esp32/main/arduino_main.cpp
@@ -109,18 +109,21 @@ void loop() {
 
   if ((timestamp - timer_50ms_timestamp) > 50)
   {
-    if (Web_Handler.isAccessTimeout() 
-        && !OTA_Handler.isOtaRunning())
+    if (Web_Handler.isRunning())
     {
-      Log_Handler.println("No Web Request since 3 Minutes - shutting down Web Server.");
-      Web_Handler.shutdown();
-      Wifi_Handler.shutdown();
-    }
-    else
-    {
-      Web_Handler.run();
-      Wifi_Handler.update();
-      OTA_Handler.run();
+      if (Web_Handler.isAccessTimeout()  
+          && !OTA_Handler.isOtaRunning())
+      {
+        Log_Handler.println("No Web Request since 3 Minutes - shutting down Web Server.");
+        Web_Handler.shutdown();
+        Wifi_Handler.shutdown();
+      }
+      else
+      {
+        Web_Handler.run();
+        Wifi_Handler.update();
+        OTA_Handler.run();
+      }
     }
 
     Preferences_Handler.run();
